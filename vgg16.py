82fdbaa2c5079b35ce9608a64f7032fe96b48fd5
Ubuntu <ubuntu@ip-172-31-43-132.us-west-2.compute.internal>
2017-04-22 04:19:59 +0000

    second

--- vgg16.py
+++ vgg16.py
@@ -2,6 +2,7 @@ import os
 import h5py
 import numpy as np
 np.random.seed(148)
+import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
 from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, Dense, GlobalAveragePooling2D
@@ -11,19 +12,20 @@ from keras.applications.vgg19 import VGG19
 from keras.applications.inception_v3 import InceptionV3
 from keras.applications.vgg16 import VGG16
 from keras import backend as K
-from keras.optimizers import SGD
+from keras.optimizers import SGD, RMSprop
 import matplotlib.pyplot as plt
 
 # path to the model weights file.
-img_width, img_height = 150, 150
+img_width, img_height = 150, 150 
 train_data_dir = 'train'
 validation_data_dir = 'validation'
 batch_size = 32
-epochs = 500
+epochs = 60
 
-num_classes = 20
+num_classes = 200
 
-datagen = ImageDataGenerator(rescale=1./255)
+datagen = ImageDataGenerator(rescale=1./255, horizontal_flip=True, rotation_range=10)
+
 base_model = VGG19(weights='imagenet', input_shape=(img_width, img_height, 3), pooling='max', include_top=False)
 
 x = base_model.output
@@ -45,25 +47,31 @@ generator_test = datagen.flow_from_directory(
         target_size=(img_width, img_height),
         batch_size=batch_size,
         class_mode='categorical',
-        shuffle=False)
+        shuffle=True)
 
 for i, layer in enumerate(base_model.layers):
    print(i, layer.name)
 
-for layer in model.layers[:17]:
+for layer in model.layers[:19]:
     print(layer.name)
     layer.trainable = False
-for layer in model.layers[17:]:
+for layer in model.layers[19:]:
    layer.trainable = True
 
 model.summary()
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.95), loss='categorical_crossentropy', metrics=['accuracy'])
+#model.compile(optimizer=RMSprop(), loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+tbCB = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=False, write_images=False)
+
 
 history = model.fit_generator(generator_train,
-    steps_per_epoch=600 / batch_size,
+    steps_per_epoch=5096 / batch_size,
     epochs=epochs,
     validation_data=generator_test,
-    validation_steps=515 / batch_size)
+    validation_steps=1024 / batch_size,
+    callbacks=[tbCB])
 
 model.save_weights('weights.h5')
 
