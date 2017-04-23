6b72b748287ad95f72a5a1034350153c9cae2801
Connor Lee <connorlee77@gmail.com>
2017-04-23 04:21:51 -0700

    stuff

---
+++ p2/ee148set3.m
@@ -0,0 +1,20 @@
+acc = csvread('run_.,tag_acc.csv', 1);
+vacc = csvread('run_.,tag_val_acc.csv', 1);
+loss = csvread('run_.,tag_loss.csv', 1);
+vloss = csvread('run_.,tag_val_loss.csv', 1);
+
+figure
+plot(acc(:,3));
+hold on;
+plot(vacc(:,3));
+xlabel('epoch');
+ylabel('accuracy');
+legend('training','validation', 'location', 'southeast')
+
+figure
+plot(loss(:,3));
+hold on;
+plot(vloss(:,3));
+xlabel('epoch');
+ylabel('cross entropy loss');
+legend('training','validation', 'location', 'northeast')
