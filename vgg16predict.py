6a55b58054fe1613db75764ca2d6fada7fea96f7
Connor Lee <connorlee77@gmail.com>
2017-04-22 23:56:37 -0700

    confusion

--- vgg16predict.py
+++ vgg16predict.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd 
 np.random.seed(148)
 
+import sklearn.metrics
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt 
+
 import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential, Model
@@ -16,26 +21,18 @@ from keras.optimizers import SGD, RMSprop
 
 from scipy import ndimage
 
-def getTestData():
 
-	DIR = './'
-	test_CSV = pd.read_csv(DIR + 'test.csv')
-	
-	X = []
-	Y = []
-	for i, row in test_CSV.iterrows():
-		path = row['path']
-		label = row['label']
-		
-		Y.append(label)
-		img = ndimage.imread(DIR + 'validation/' +path)
-		X.append(img)
+def plot_confusion_matrix(cm):
+    
+    plt.imshow(cm, interpolation='nearest', cmap='hot')
+    plt.colorbar()
 
-	return X, np.array(Y)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('confusionmatrix1')
 
-x, y = getTestData()
-print x
-print y
+
+
 # path to the model weights file.
 img_width, img_height = 224, 224 
 train_data_dir = 'train'
@@ -45,10 +42,12 @@ validation_data_dir = 'validation'
 ### Parameters
 batch_size = 32
 num_classes = 200
-test_size = 512
+total_test = 5794
+steps = int(128/batch_size)
 
-
-
+DIR = './'
+test_CSV = pd.read_csv(DIR + 'test.csv')
+true_labels = test_CSV['label'].values[0:steps * batch_size]
 
 testgen = ImageDataGenerator(rescale=1./255)
 
@@ -67,9 +66,7 @@ x = Dense(512, activation='relu')(x)
 x = Dropout(0.5)(x)
 predictions = Dense(num_classes, activation='softmax')(x)
 
-DIR = './'
-test_CSV = pd.read_csv(DIR + 'test.csv')
-true = test_CSV['label'].values[0:32]
+
 
 # Combined model w/ classifier
 model = Model(input=base_model.input, output=predictions)
@@ -77,7 +74,9 @@ model.summary()
 model.compile(optimizer=RMSprop(), loss='categorical_crossentropy', metrics=['accuracy'])
 model.load_weights('total_weights.h5')
 
-predictions = model.predict_generator(generator_test, 1, verbose=1)
-p = np.array(map(np.argmax, predictions)) + 1 
+predictions = model.predict_generator(generator_test, steps, verbose=1)
+pred_labels = np.array(map(np.argmax, predictions)) + 1
 
-
+acc = sklearn.metrics.accuracy_score(true_labels, pred_labels)
+cm = sklearn.metrics.confusion_matrix(true_labels, pred_labels, labels=[i for i in range(1, 201)])
+plot_confusion_matrix(cm)
